6fca8e47d9a54ae6191845a32de895c8f797da1f
reinka <andrei.poehlmann90@gmail.com>
2015-11-11 13:22:47 +0100

    Aufgabe 1 - Largest pandigital prime

--- blatt4/a1.py
+++ blatt4/a1.py
@@ -21,7 +21,7 @@ def isPrime(x):
 
 	return prime
 
-#Test isPrime(x)
+#Testing isPrime(x)
 #for num in range(0, 100):
 #	print str(num) + ' ' + str(isPrime(num)) 
 
@@ -36,7 +36,7 @@ def biggest_pan(n):
 
 	return biggest
 
-#Test biggest_num(n)
+#Testing biggest_num(n)
 #print biggest_pan(n) 
 
 #Function calculating the n-th digit of a number
@@ -47,13 +47,13 @@ def nth_digit(x, n):
 		n -= 1
 	return num
 
-#Test nth_digit(x,n)
+#Testing nth_digit(x,n)
 #for num in range(1, n+1):
 #	print nth_digit(biggest_pan(n), num)
 
 
-#print isPrime(2143)
 
+#Function calculating the digit sum of a number x
 def digit_sum(x):
 	temp = x
 	dig_sum = 0
@@ -62,9 +62,10 @@ def digit_sum(x):
 		x /= 10
 		dig_sum
 	return dig_sum
-
+#Testing digit_sum(x)
 #print digit_sum(12345)
 
+#Function which checks if a digit of x is within range [1-n]
 def is_range(x):
 	in_range = True
 	for i in range(1, n+1):
@@ -73,35 +74,40 @@ def is_range(x):
 		else:
 			in_range = False
 	return in_range
+#Testing is_range(x)
 #n = 4
 #print is_range(4235)
 
+#Function which checks if a digit is being repeated within a n-digital number
 def repeat_digit(x):
 	no_repeat = True
 	for num in range(1, n+1):
 		if str(num) not in str(x):
 			no_repeat = False
 	return no_repeat
-
+#Testing repeat_digit(x)
 #n = 5
 #print repeat_digit(12245)
 
-def main():
+
+#Main function calculating the largest pandigital prime, given n, which will be 
+#asked as input
+def largest_pandigital_prime():
 	global n
-	n = input("Choose n:")
+	n = input("Choose n (range 1-9):")
 	while(biggest_pan(n) % 3 == 0):
 		n -= 1
 	
 	while(n>1):
 		x = biggest_pan(n)
-		print "biggest pan:", x
-		print "n:", n
+		print "Largest pandigital number:", x
+		print "n =", n
 		while(x>0):
 			if(isPrime(x)):
 				if(is_range(x)):
 					if(digit_sum(x) == digit_sum(biggest_pan(n))):
 						if(repeat_digit(x)):
-							print "Biggest pandigital number found: ",x
+							print "Largest pandigital prime number:",x
 							n =- 1
 							break
 						else:
@@ -122,6 +128,6 @@ def main():
 
 
 
-main()
+largest_pandigital_prime()
 		
 
